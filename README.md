94979414493ab2b693e3215876e4df2efb0880de
kadw108 <wallwritinglast@gmail.com>
2023-01-25 10:47:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Saint Can Passage
+
+by KADW (KADW#4425)
+
+Version 1.0
+
+A small mod for Rain World 1.9 that allows Saint to use passages. For all the lazy players out there.
+
+Available through Steam Workshop/Remix at [this link](https://steamcommunity.com/sharedfiles/filedetails/?id=2923294502), so get it from there. Commented source files are in this repo.
+
+If you don't have Rain World through Steam, you can still install this mod by clicking the green Code button, then Download ZIP. Move SaintCanPassage_final into your Rain World/RainWorld_Data/StreamingAssets/mods folder.
---
+++ SaintCanPassageCode.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using UnityEngine;
+
+// mod-specific using
+using Menu;
+using MoreSlugcats;
+
+namespace SaintCanPassage
+{
+    [BepInPlugin("kadw.saintcanpassage", "Saint Can Passage", "1.0")]
+    public class SaintCanPassageCode : BaseUnityPlugin
+    {
+        public void OnEnable()
+        {
+            On.Menu.SleepAndDeathScreen.AddPassageButton += SleepAndDeathScreen_AddPassageButton;
+        }
+
+        private static void SleepAndDeathScreen_AddPassageButton(On.Menu.SleepAndDeathScreen.orig_AddPassageButton orig, SleepAndDeathScreen self, bool buttonBlack)
+        {
+            orig(self, buttonBlack);
+
+            if (self.saveState != null && ModManager.MSC && self.saveState.saveStateNumber == MoreSlugcatsEnums.SlugcatStatsName.Saint)
+            {
+                Debug.Log("Saint Can Passage mod: Add passage button.");
+                self.passageButton = new SimpleButton(self, self.pages[0], self.Translate("PASSAGE"), "PASSAGE", new Vector2(self.LeftHandButtonsPosXAdd + self.manager.rainWorld.options.SafeScreenOffset.x, Mathf.Max(self.manager.rainWorld.options.SafeScreenOffset.y, 15f)), new Vector2(110f, 30f));
+                self.pages[0].subObjects.Add(self.passageButton);
+                if (buttonBlack)
+                {
+                    self.passageButton.black = 1f;
+                }
+                self.passageButton.lastPos = self.passageButton.pos;
+            }
+        }
+    }
+}
